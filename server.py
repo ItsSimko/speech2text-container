8c0d4fc959c04ca4e3e37372b52b1ef6a127d0d4
Alex Simko <asimko5312@conestogac.on.ca>
2024-10-03 11:30:55 -0400

    Moved argparser to a seperate function + moved functions to utils.py file

--- server.py
+++ server.py
@@ -9,50 +9,26 @@
 # and Research Students - Software Developer Alex Simko, Pemba Sherpa (F24), and Naitik Patel.
 
 from torch._C import NoneType
-from fastapi import FastAPI, File, UploadFile, HTTPException, Security, Depends
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials 
+from fastapi import FastAPI, File, UploadFile, HTTPException, Security
 from pydantic import BaseModel
 from dotenv import load_dotenv
+from utils import check_api_key, get_api_key, parse_arguments
 import whisper
 import uvicorn
 import os
 import tempfile
-import argparse
-import secrets
 import magic
 
 load_dotenv()
+sessionApiKey = check_api_key()
 
 # Initialize Whisper model
 model = NoneType
-sessionApiKey = NoneType
-
 app = FastAPI()
-bearer_scheme = HTTPBearer()
 
 class TranscriptionResponse(BaseModel):
     text: str
 
-def generate_api_key():
-    return secrets.token_urlsafe(32)
-
-def get_api_key(credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme)) -> str:
-    token = credentials.credentials
-    if token == sessionApiKey:
-        return token
-    raise HTTPException(
-        status_code=401,
-        detail="Invalid or missing Bearer Token",
-    )
-
-def check_api_key():
-    api_key = os.getenv('SESSION_API_KEY')
-
-    if not api_key:
-        # If not found, generate a new one and every session
-        api_key = generate_api_key()
-    
-    return api_key
 
 @app.post("/whisperaudio", response_model=TranscriptionResponse)
 async def transcribe_audio(file: UploadFile = File(...), api_key: str = Security(get_api_key)):
@@ -85,18 +61,14 @@ async def transcribe_audio(file: UploadFile = File(...), api_key: str = Security
 
     return response_data
 
-if __name__ == '__main__':
-    # Command-line argument parsing
-    parser = argparse.ArgumentParser(description="FastAPI server for Whisper audio transcription")
-    parser.add_argument("--host", type=str, default="0.0.0.0", help="Host address to run the server on")
-    parser.add_argument("--port", type=int, default=2224, help="Port number to run the server on")
-    parser.add_argument("--whispermodel", type=str, default="medium", help="Whisper model to use for transcription (https://github.com/openai/whisper?tab=readme-ov-file#available-models-and-languages)")
 
-    args = parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_arguments()
 
     model = whisper.load_model(args.whispermodel)
     sessionApiKey = check_api_key()
 
-    print(f"Use this API key for requests: {sessionApiKey}")
+    print(f"Use this API key for requests with bearer header: {sessionApiKey}")
 
     uvicorn.run(app, host=args.host, port=args.port)
